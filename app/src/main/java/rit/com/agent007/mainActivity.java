f50f7e6d37d7f70a8acad2f83f37833d2b62a302
stefanmiljakovic <stefanmiljakovic@yahoo.com>
2017-10-19 20:27:57 +0200

    Complete

--- app/src/main/java/rit/com/agent007/mainActivity.java
+++ app/src/main/java/rit/com/agent007/mainActivity.java
@@ -4,16 +4,25 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -23,6 +32,11 @@ public class mainActivity extends AppCompatActivity {
 
     private LocationManager locationManager;
     private LocationListener locationListener;
+    private SensorEventListener gyroscopeEventListener;
+    private Sensor gyroscopeSensor;
+    private SensorManager sensorManager;
+
+
 
 
     private Typeface getTypeface() {
@@ -47,6 +61,61 @@ public class mainActivity extends AppCompatActivity {
         locationManager.requestLocationUpdates("gps", 7000, 0, locationListener);
     }
 
+    private void switchVisibility(View v){
+        if(v.getVisibility() == View.INVISIBLE)
+            v.setVisibility(View.VISIBLE);
+        else
+            v.setVisibility(View.INVISIBLE);
+
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+        r.play();
+
+    }
+    private void implementSensor(){
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        final TextView textViewHide2 = (TextView)findViewById(R.id.textViewGPSEnabled);
+        final TextView textViewHide1 = (TextView)findViewById(R.id.textViewLocation);
+
+        if(gyroscopeSensor==null)
+            finish();
+
+        gyroscopeEventListener = new SensorEventListener(){
+
+            private int rotationCount = 1;
+            private boolean switchRotationCount = true;
+
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if(event.values[1] > 3.0f && switchRotationCount){
+                    rotationCount++;
+                    switchRotationCount = !switchRotationCount;
+                }
+                else if(event.values[1] < -3.0f && !switchRotationCount){
+                    rotationCount++;
+                    switchRotationCount = !switchRotationCount;
+                }
+
+                if(rotationCount % 7 == 0){
+                    switchVisibility(textViewHide1);
+                    switchVisibility(textViewHide2);
+
+                    rotationCount = 1;
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+        };
+
+        sensorManager.registerListener(gyroscopeEventListener, gyroscopeSensor, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         runFullscreen();
@@ -66,6 +135,7 @@ public class mainActivity extends AppCompatActivity {
 
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         locationListener = new LocationListener() {
+
             @Override
             public void onLocationChanged(Location location) {
                 textView.setText("Welcome agent " + agentName + "\n\nLatitude: " + location.getLatitude() + "\nLongitude: " + location.getLongitude());
@@ -115,6 +185,8 @@ public class mainActivity extends AppCompatActivity {
                 textViewGPS.setText("GPS Enabled");
             }}
 
+        implementSensor();
+
     }
 
     @Override
@@ -135,6 +207,18 @@ public class mainActivity extends AppCompatActivity {
                     regLocationListener();
                 return;
         }
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        sensorManager.registerListener(gyroscopeEventListener, gyroscopeSensor, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(gyroscopeEventListener);
     }
 
 }
